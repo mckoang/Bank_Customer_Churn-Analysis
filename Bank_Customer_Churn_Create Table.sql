6a0e307785b46a5ed966d8a8af81f5948b1f5a82
Michael Konadu Ansong <98789196+mckoang@users.noreply.github.com>
2025-04-20 22:31:52 +0000

    Create Table

---
+++ Bank_Customer_Churn_Create Table.sql
@@ -0,0 +1,13 @@
+Create Table customer_churn(
+    RowNumber int,
+	CustomerId int,
+	Surname varchar(50),
+	CreditScore int,
+	Geography varchar(50),
+	Gender varchar(15)
+	Age int,
+	Tenure int,
+	Balance decimal,
+	
+
+)
